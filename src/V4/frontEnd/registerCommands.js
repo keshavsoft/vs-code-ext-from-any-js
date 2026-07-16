8c439409d49829f01d86b7fc803550c84714cbb2
keshavsoft <nknnkumar@yahoo.com>
2026-07-16 13:49:24 +0530

    add list perfect

--- src/V4/frontEnd/registerCommands.js
+++ src/V4/frontEnd/registerCommands.js
@@ -1,15 +1,7 @@
-import * as vscode from 'vscode';
-
-import readHtml from './addListeners/backend/readHtml.js';
+import addListenersCommands from './addListeners/registerCommands.js';
 
 const registerAllCommands = (context) => {
-    // 2. Register new Webview command (AddTableNameHtml)
-    const commandHtml = "extension.editor.title.frontend.addListeners.addHtmlId";
-
-
-    const showHtml = vscode.commands.registerCommand(commandHtml, (uri) => readHtml(context, uri));
-    
-    context.subscriptions.push(showHtml);
+    addListenersCommands(context);
 };
 
 export default registerAllCommands;
