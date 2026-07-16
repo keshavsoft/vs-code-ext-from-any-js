92c149182d083feb07f2436d7d75a85abdd0ffbe
keshavsoft <nknnkumar@yahoo.com>
2026-07-16 11:36:47 +0530

    to test

--- src/V2/nodeJs/registerCommands.js
+++ src/V2/nodeJs/registerCommands.js
@@ -1,12 +1,25 @@
 import * as vscode from 'vscode';
 
-import readHtml from './routesJs/backend/readHtml.js';
+import routesJs from './routesJs/backend/readHtml.js';
+import appJs from './appJs/backend/readHtml.js';
 
-const registerAllCommands = (context) => {
+const routesJsCommands = (context) => {
     // 2. Register new Webview command (AddTableNameHtml)
     const commandHtml = 'extension.editor.title.routesjs.addTableNameHtml';
-    const showHtml = vscode.commands.registerCommand(commandHtml, (uri) => readHtml(context, uri));
+    const showHtml = vscode.commands.registerCommand(commandHtml, (uri) => routesJs(context, uri));
     context.subscriptions.push(showHtml);
+};
+
+const appJsCommands = (context) => {
+    // 2. Register new Webview command (AddTableNameHtml)
+    const commandHtml = 'extension.editor.title.routesjs.addTableNameHtml';
+    const showHtml = vscode.commands.registerCommand(commandHtml, (uri) => appJs(context, uri));
+    context.subscriptions.push(showHtml);
+};
+
+const registerAllCommands = (context) => {
+    routesJsCommands(context);
+    appJsCommands(context);
 };
 
 export default registerAllCommands;
