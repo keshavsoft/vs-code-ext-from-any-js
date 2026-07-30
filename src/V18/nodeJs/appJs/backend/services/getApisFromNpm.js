edf71e9eedcb496b652edb37fe05fe7b0cd3a228
keshavsoft <nknnkumar@yahoo.com>
2026-07-30 07:25:28 +0530

    any js still working

--- src/V18/nodeJs/appJs/backend/services/getApisFromNpm.js
+++ src/V18/nodeJs/appJs/backend/services/getApisFromNpm.js
@@ -1,7 +1,17 @@
-import extractApis from 'pattern-collector-appjs-use-extract';
+// import extractApis from 'pattern-collector-appjs-use-extract';
+import getAnyJsStory from 'pattern-collector-anyjs-story';
 
 const getSchemaFiles = (activeEditorContect) => {
-    const apisPresent = extractApis({ fileContent: activeEditorContect });
+    // const apisPresent = extractApis({ fileContent: activeEditorContect });
+    const anyJsStory = getAnyJsStory({
+        fileContent: activeEditorContect,
+        fileType: "fromAppJs"
+    });
+
+    // console.log("aaaaaaaa : ", anyJsStory.linesStory);
+    const apisPresent = anyJsStory?.linesStory?.importLines.map(element => {
+        return element?.part2;
+    });
 
     return apisPresent;
     // return getTableNames(inTargetPath);
